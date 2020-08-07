604860578471f0d2af63842e2453b8f4fa658118
lkomanetz <lkomanetz@mii.com>
2020-08-07 09:28:06 -0500

    Adds AsyncCommand and Task Extensions.

---
+++ src/dotnet/Extensions/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Dtk.Extensions {
+
+    public static class TaskExtensions {
+
+		public static async void FireAndForgetSafe(this Task task, Action<Exception> onError, Action onFinally) {
+			try {
+				await task;
+			}
+			catch (Exception err) {
+				onError(err);
+			}
+			finally {
+				onFinally();
+			}
+		}
+
+    }
+
+}
---
+++ src/dotnet/UI/AsyncCommand.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Dtk.UI {
+
+	public interface IAsyncCommand : ICommand {
+		Task ExecuteAsync(object parameter);
+		bool CanExecute();
+	}
+	
+	public abstract class AsyncCommandBase : IAsyncCommand {
+
+		private readonly Action<Exception> _errorHandler;
+
+		public AsyncCommandBase(Action<Exception> errorHandler) => _errorHandler = errorHandler;
+
+		public event Eventhandler CanExecuteChanged = delegate { };
+
+		private bool _isRunning;
+		public bool IsRunning {
+			get => _isRunning;
+			private set {
+				if (_isRunning == value) return;
+				_isRunning = value;
+				CanExecuteChanged(this, EventArgs.Empty);
+			}
+		}
+
+		public bool CanExecute() => !IsRunning;
+		bool ICommand.CanExecute(object parameter) => CanExecute();
+
+		void ICommand.Execute(object parameter) {
+			if (!CanExecute()) return;
+			IsRunning = true;
+			ExecuteAsync(parameter).FireAndForgetSafe(_errorHandler, () => IsRunning = false);
+		}
+
+		public abstract Task ExecuteAsync(object parameter);
+		
+	}
+
+	public class AsyncDelegateCommand<T> : AsyncCommandBase {
+		
+
+
+	}
+
+	public class AsyncDelegateCommand : AsyncCommandBase {
+
+		private readonly Func<Task> _func;
+
+		public AsyncDelegateCommand(Func<task func, Action<Exception> errorHandler) : base(errorHandler) =>
+			_func = func;
+
+		public override Task ExecuteAsync(object parameter) => _func();
+
+	}
+
+}
